d654c0535f9b119a91202f32691ca4705321e00b
Claudia Mengoni <43252093+Cengoni@users.noreply.github.com>
2023-02-23 17:40:58 +0100

    update setup.sh with output_dir + cores parameters

--- setup.sh
+++ setup.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
-rm -rf /PanDelos-fragments/ref_prok_rep_genomes && mkdir /PanDelos-fragments/ref_prok_rep_genomes && cd /PanDelos-fragments/ref_prok_rep_genomes
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/ref_prok_rep_genomes.??.tar.gz
+
+if [ "$#" -ne 2 ]
+then
+	echo "Illegal number of parameters. Usage ./setup.sh <db_outdir> <n_cores>"
+	exit 1
+fi 
+
+outdir=$1
+ftp_path=ftp://ftp.ncbi.nlm.nih.gov/blast/db
+rm -rf $outdir && mkdir $outdir && cd $outdir
+
+cores=$2
+
+if [ $cores -ne 1 ]
+then
+	echo 'Downloading files...'
+	wget --spider ${ftp_path}/ref_prok_rep_genomes.??.tar.gz >> tmp1 2>&1
+
+	cat tmp1 | grep "ref_prok_rep_genomes\...\.tar\.gz..exists" | grep -o 'ref_prok_rep_genomes\...\.tar\.gz' > tmp2
+	cat tmp2 | while read line; do echo ${ftp_path}/$line >> tmp3; done
+	cat tmp3 | xargs -n 1 -P $cores wget -q
+	rm tmp1 tmp2 tmp3
+  
+else
+
+	wget ${ftp_path}/ref_prok_rep_genomes.??.tar.gz
+
+fi
+
 for f in *.tar.gz
 do
-tar -xvf "$f"
+  tar -xvf "$f"
 done
 rm *.tar.gz
+
 
 # cd .. && rm -rf ref_viruses_rep_genomes && mkdir ref_viruses_rep_genomes && cd ref_viruses_rep_genomes
 # wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/ref_viruses_rep_genomes.tar.gz
